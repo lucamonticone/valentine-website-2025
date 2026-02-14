915037db6f86baae6154da803fb069e78b9b33d8
Luca Monticone <luca.monticone29@gmail.com>
2026-02-14 19:49:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yeeee! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        message: "Now come get your gift! a big warm hug and a huge kiss!",
         emojis: "🎁💖🤗💝💋❤️💕✨🌹"  // These will bounce around
     },
 
