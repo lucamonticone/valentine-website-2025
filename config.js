894d20119e861e36fb6aab8774dc3830b749580a
Luca Monticone <luca.monticone29@gmail.com>
2026-02-14 19:44:13 +0100

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
-        hearts: ['❤️', '💖', '💝', '💗', '💓', '💞', ✨', '🌹','🌙'],  // Heart emojis
+        hearts: ['❤️', '💖', '💝', '💗', '💓', '💞', '✨', '🌹','🌙'],  // Heart emojis
         bears: ['🐶','🐾','🦴','💫']                       // Cute bear emojis
     },
 
